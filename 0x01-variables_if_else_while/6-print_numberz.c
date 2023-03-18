bdedf6d48faec6befe413f3dfb8fbd127005620a
Better-Call-Frank <franklinemogaka@gmail.com>
2023-03-17 17:43:50 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Good)
+ */
+
+int main(void)
+
+{
+	int f;
+	
+	for (f = '0' ; f <= '9' ; f++)
+	putchar(f);
+	putchar('\n');
+
+	return (0);
+
+}
