da226107ef24f0c5521b35a85049210f43864cb3
Better-Call-Frank <franklinemogaka@gmail.com>
2023-04-05 13:35:38 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,8 +3,9 @@
 /**
  * _puts_recursion - Prints string followed by new line
  * @s: Input
- * Return: Alwaays 0 (Success)
+ * Return: Always 0 (Success)
  */
+void _puts_recursion(char *s);
 {
 	if (*s)
 	{
