5e59ed7ff653c59713d493de31840c4d09b77630
Better-Call-Frank <franklinemogaka@gmail.com>
2023-04-05 14:34:19 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if string is palindrome
+ * @s: string to check
+ * Return: 1 if it is, 0 it's not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+
+}
