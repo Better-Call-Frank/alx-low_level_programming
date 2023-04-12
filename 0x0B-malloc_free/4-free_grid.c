42593871b02b36a94c2f1744f9d2a83e58468217
Better-Call-Frank <franklinemogaka@gmail.com>
2023-04-11 17:59:35 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -12,14 +12,11 @@
  */
 void free_grid(int **grid, int height)
 {
-    if (grid == NULL || height <= 0) {
-        return;
-    }
+	int i;
 
-    for (int i = 0; i < height; i++) {
-        free(grid[i]);
-        grid[i] = NULL;
-    }
-    free(grid);
-    grid = NULL;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
