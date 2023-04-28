b7f582b36dba0ace4ec48710d0f7bffe4ac67c4b
Better-Call-Frank <franklinemogaka@gmail.com>
2023-04-27 17:53:40 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,14 +4,12 @@ void print_first_sentence(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 }
-
 void print_second_sentence(void)
 {
-    printf("I bore my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }
-
 void __attribute__((constructor)) run_before_main()
 {
-    print_first_sentence();
-    print_second_sentence();
+	print_first_sentence();
+	print_second_sentence();
 }
