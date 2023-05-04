7b8a9f1752733f885b1c366db55dacfb528895e3
Better-Call-Frank <franklinemogaka@gmail.com>
2023-05-04 12:33:25 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,28 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (b == NULL)
-	{
+	if (!b)
 		return (0);
-	}
 
-	for (int i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] == '0')
-		{
-			result = result << 1;
-		}
-		else if (b[i] == '1')
-		{
-			result = (result << 1) | 1;
-		}
-		else
-		{
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		}
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (result);
+	return (dec_val);
 }
